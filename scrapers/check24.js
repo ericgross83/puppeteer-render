55702d004b83bc2b9a3255e59b6b45dbaba4cc8e
Eric Groß <ericgross.eg@gmail.com>
2026-03-09 18:38:34 +0100

    refactor: Set puppeteer to headless mode in Check24 scraper

--- scrapers/check24.js
+++ scrapers/check24.js
@@ -18,7 +18,7 @@ const scrapeCheck24 = async (data) => {
     let browser;
     try {
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: [
                 '--no-sandbox', 
                 '--disable-setuid-sandbox', 
